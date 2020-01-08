ed4ef6c7d5115b5a8eb77854879ccab34c0d0562
rengreen <r.m.gardzinska@gmail.com>
2020-01-08 21:27:33 +0100

    Spring Initializr

---
+++ src/test/java/pl/rengreen/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rengreen.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
