938f0386750dcaa9d2982b3667b5ff9c45af04f4
rengreen <r.m.gardzinska@gmail.com>
2020-01-13 20:37:41 +0100

    Create endpoint for designing taco with multiple checkboxes

---
+++ src/main/java/pl/rengreen/tacocloud/HomeController.java
@@ -0,0 +1,15 @@
+package pl.rengreen.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showHome(){
+        return "home";
+    }
+
+}
---
+++ src/main/java/pl/rengreen/tacocloud/web/DesignTacoController.java
@@ -0,0 +1,49 @@
+package pl.rengreen.tacocloud.web;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+import pl.rengreen.tacocloud.Ingredient;
+import pl.rengreen.tacocloud.Ingredient.Type;
+import pl.rengreen.tacocloud.Taco;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "pszenna", Type.WRAP),
+                new Ingredient("COTO", "kukurydziana", Type.WRAP),
+                new Ingredient("GRBF", "mielona wołowina ", Type.PROTEIN),
+                new Ingredient("CARN", "kawałki mięsa", Type.PROTEIN),
+                new Ingredient("TMTO", "pomidory pokrojone w kostkę", Type.VEGGIES),
+                new Ingredient("LETC", "sałata", Type.VEGGIES),
+                new Ingredient("CHED", "cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "pikantny sos pomidorowy", Type.SAUCE),
+                new Ingredient("SRCR", "śmietana", Type.SAUCE)
+        );
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    ingredients.stream()
+                            .filter(ingredient -> type.equals(ingredient.getType()))
+                            .collect(Collectors.toList()));
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+}
